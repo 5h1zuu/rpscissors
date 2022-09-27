97c7b5cb80b83194a52a9e9a34d89b6f23a1c66b
Shizuu <coolcoledude111@gmail.com>
2022-09-27 14:01:24 +0100

    javascript.js

--- javascript.js
+++ javascript.js
@@ -1,13 +1,5 @@
 function getComputerChoice(){
     let computer =  Math.floor(Math.random() * 4)
-   
-        if (computer === 1){
-            console.log("Rock");
-        }else if (computer === 2){
-            console.log("Paper");
-        }else if (computer === 3){
-            console.log("Scissors")
-        }
 }
 
 getComputerChoice()
@@ -17,6 +9,21 @@ getComputerChoice()
 let playerSelection = prompt("Type rock, paper or scissors")
 let playerSelect = playerSelection.toLowerCase()
 
-function playRound(){
+if (playerSelect == "rock"){
+    playerSelect = 1;
+    
+}else if (playerSelect == "paper"){
+    playerSelect = 2;
+    
+}else if (playerSelect == "scissors"){
+    playerSelect = 3;
+}else{
+    console.log ("error did not type rock, paper or scissors")
+}
 
+
+
+
+function playRound(playerSelect, getComputerChoice){
+    
 }
