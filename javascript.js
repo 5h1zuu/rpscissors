b8c383fe35f96ba4a74a67f31c8e57894d44f91d
Shizuu <coolcoledude111@gmail.com>
2022-09-27 13:42:15 +0100

    javascript.js

---
+++ javascript.js
@@ -0,0 +1,10 @@
+let rock = 1;
+let paper = 2;
+let scissors = 3;
+
+function getComputerChoice(){
+    let computer =  Math.floor(Math.random() * 4)
+    console.log(computer)
+}
+
+getComputerChoice()
