4d1e9ba511136fd3e2569e799f5290e322cc3104
Shizuu <coolcoledude111@gmail.com>
2022-09-28 15:09:14 +0100

    javascript.js

--- javascript.js
+++ javascript.js
@@ -1,3 +1,8 @@
+
+function game(){
+    for(let x = 0; x < 5; x++){
+
+
 // need to get the computer choice
 function getComputerChoice(){
     let choice = Math.floor(Math.random() * 3)
@@ -10,48 +15,53 @@ function getComputerChoice(){
     }
 }
 
+let computerChoice = getComputerChoice()
+
+
 // need to get the user choice
-function getUserChoice(){
+//function getUserChoice(){
     let user = prompt("Choose Rock, Paper or Scissors")
-    let userChoice = user.toLowerCase()
-    if (userChoice == "rock" || userChoice == "paper" || userChoice == "scissors"){
+    let getUserChoice = user.toLowerCase()
+ /*   if (userChoice == "rock"){
+        return userChoice
+    }else if (userChoice == "paper"){
+        return userChoice 
+    }else if(userChoice == "scissors"){
         return userChoice
     }else{
         return "error, reload page"
     }
 
-}
+} */
 
 
 
 
 // need to create the conditions for the game
 function playRound(){
-    if (getComputerChoice() == getUserChoice()){
+    if (computerChoice == getUserChoice){
         return "draw"
 
-    }else if (getUserChoice() == "rock" && getComputerChoice() == "scissors"){
+    }else if (getUserChoice === "rock" && computerChoice === "scissors"){
         return "User won the round"
 
-    }else if (getUserChoice() == "rock" && getComputerChoice() == "paper"){
+    }else if (getUserChoice === "rock" && computerChoice === "paper"){
         return "Computer won the round"
 
-    }else if (getUserChoice() == "paper" && getComputerChoice() == "rock"){
+    }else if (getUserChoice === "paper" && computerChoice === "rock"){
         return "User won the round"
 
-    }else if (getUserChoice() == "paper" && getComputerChoice() == "scissors"){
+    }else if (getUserChoice === "paper" && computerChoice === "scissors"){
         return "Computer won the round"
 
-    }else if (getUserChoice() == "scissors" && getComputerChoice() == "paper"){
+    }else if (getUserChoice === "scissors" && computerChoice === "paper"){
         return "User won the round"
 
-    }else if (getUserChoice() == "scissors" && getComputerChoice() == "rock"){
+    }else if (getUserChoice === "scissors" && computerChoice === "rock"){
         return "Computer won the round"
     }
 
 }
-
-
 
 
 
@@ -61,7 +71,7 @@ function playRound(){
 
 
 // need a score system
-let userScore = 0
+/*let userScore = 0
 let compScore = 0
 
 function score(){
@@ -78,21 +88,27 @@ function score(){
     }
 }
 
-
-
-
-
+*/
 
 
 
 
 // need to loop the game 5 times
+console.log(computerChoice)
+console.log(getUserChoice)
+console.log(playRound())
+
+
+
+
+    }
+}
 
 
 
 
 
-
+game()
 
 
 
