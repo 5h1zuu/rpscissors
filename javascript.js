c0c8515e31424e268699caa10bacef90f141e180
Shizuu <coolcoledude111@gmail.com>
2022-09-28 14:25:14 +0100

    javascript.js

--- javascript.js
+++ javascript.js
@@ -10,28 +10,46 @@ function getComputerChoice(){
     }
 }
 
-console.log(getComputerChoice())
-
-
-
-
-
-
-
 // need to get the user choice
+function getUserChoice(){
+    let user = prompt("Choose Rock, Paper or Scissors")
+    let userChoice = user.toLowerCase()
+    if (userChoice == "rock" || userChoice == "paper" || userChoice == "scissors"){
+        return userChoice
+    }else{
+        return "error, reload page"
+    }
 
-
-
-
-
+}
 
 
 
 
 // need to create the conditions for the game
+function playRound(){
+    if (getComputerChoice() == getUserChoice()){
+        return "draw"
 
+    }else if (getUserChoice() == "rock" && getComputerChoice() == "scissors"){
+        return "User won the round"
 
+    }else if (getUserChoice() == "rock" && getComputerChoice() == "paper"){
+        return "Computer won the round"
 
+    }else if (getUserChoice() == "paper" && getComputerChoice() == "rock"){
+        return "User won the round"
+
+    }else if (getUserChoice() == "paper" && getComputerChoice() == "scissors"){
+        return "Computer won the round"
+
+    }else if (getUserChoice() == "scissors" && getComputerChoice() == "paper"){
+        return "User won the round"
+
+    }else if (getUserChoice() == "scissors" && getComputerChoice() == "rock"){
+        return "Computer won the round"
+    }
+
+}
 
 
 
@@ -43,8 +61,22 @@ console.log(getComputerChoice())
 
 
 // need a score system
+let userScore = 0
+let compScore = 0
 
+function score(){
+    if (playRound() == "User won the round"){
+        userScore = userScore + 1
+        return userScore
 
+    }else if (playRound() == "Computer won the round"){
+        compScore = compScore + 1
+        return compScore
+
+    }else{
+        return "draw"
+    }
+}
 
 
 
