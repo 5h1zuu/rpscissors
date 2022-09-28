dc92ed7d31f4006b9ce071a00e10b914f6909cd9
Shizuu <coolcoledude111@gmail.com>
2022-09-28 11:19:45 +0100

    javascript.js

--- javascript.js
+++ javascript.js
@@ -19,43 +19,52 @@ computerSelection = getComputerChoice()
 player = prompt("rock, paper or scissors")
 playerSelection = player.toLowerCase()
 
-console.log(playerSelection)
 
 
 function playRound(playerSelection, computerSelection){
+
     if (playerSelection == computerSelection){
+        console.log("draw")
         return "draw"
+    
     }if (playerSelection == "rock" && computerSelection == "scissors"){
+        console.log("player wins")
         return "player wins"
 
     }else if (playerSelection == "rock" && computerSelection == "paper"){
+        console.log("computer wins")
         return "computer wins"
 
     }else if (playerSelection == "paper" && computerSelection == "rock"){
+        console.log("player wins")
         return "player wins"
 
     }else if (playerSelection == "paper" && computerSelection == "scissors"){
+        console.log("computer wins")
         return "computer wins"
-    
+            
     }else if (playerSelection == "scissors" && computerSelection == "paper"){
+        console.log("player wins")
         return "player wins"
 
     }else if(playerSelection == "scissors" && computerSelection == "rock"){
+        console.log("computer wins")
         return "computer wins"
+            }
     }
-}
-
-playRound(playerSelection, computerSelection)
+    
 
 
 
 function game(){
-    for (let x = 0; x < 5; x++){
-        console.log(x)
-        return
+    for (let i = 0; i < 5; i++){
+        getComputerChoice()
+        player = prompt("rock, paper or scissors")
+        playerSelection = player.toLowerCase()
+        playRound(playerSelection, computerSelection)
+        
     }
 }
-
 
 
 game()
