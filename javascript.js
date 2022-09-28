c8d8655685a23d211cdec0ca69167a2d9a06fe06
Shizuu <coolcoledude111@gmail.com>
2022-09-28 10:54:44 +0100

    javascript.js

--- javascript.js
+++ javascript.js
@@ -3,15 +3,12 @@ function getComputerChoice(){
     let choice = Math.floor(Math.random() * 3)
     if (choice === 0){
         choice = "rock";
-        console.log(choice);
         return choice;
     }else if (choice === 1){
         choice = "paper";
-        console.log(choice);
         return choice;
     }else if (choice === 2 ){
         choice = "scissors"
-        console.log(choice)
         return choice;
     }
 }
@@ -27,30 +24,23 @@ console.log(playerSelection)
 
 function playRound(playerSelection, computerSelection){
     if (playerSelection == computerSelection){
-        console.log("draw")
         return "draw"
     }if (playerSelection == "rock" && computerSelection == "scissors"){
-        console.log("player wins")
         return "player wins"
 
     }else if (playerSelection == "rock" && computerSelection == "paper"){
-        console.log("computer wins")
         return "computer wins"
 
     }else if (playerSelection == "paper" && computerSelection == "rock"){
-        console.log("player wins")
         return "player wins"
 
     }else if (playerSelection == "paper" && computerSelection == "scissors"){
-        console.log("computer wins")
         return "computer wins"
     
     }else if (playerSelection == "scissors" && computerSelection == "paper"){
-        console.log("player wins")
         return "player wins"
 
     }else if(playerSelection == "scissors" && computerSelection == "rock"){
-        console.log("computer wins")
         return "computer wins"
     }
 }
