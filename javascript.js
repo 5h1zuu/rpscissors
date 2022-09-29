a3741b833a0d93e54168f0aef5f37b6216981aa5
Shizuu <coolcoledude111@gmail.com>
2022-09-29 11:48:49 +0100

    javascript.js

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 let compScore = 0
 let userScore = 0
-
+console.log(`User points: ${userScore} Computer points: ${compScore}`)
 
 // need to get the computer choice
 function getComputerChoice(){
@@ -14,16 +14,9 @@ function getComputerChoice(){
     }
 }
 
-
-
-
 // need to create a function
 // need to get the user choice
 //function getUserChoice for validation
-
-
-
-
 
 // need to create the conditions for the game
 let round = function playRound(){
@@ -31,72 +24,62 @@ let round = function playRound(){
     let user = prompt("Choose Rock, Paper or Scissors")
     let getUserChoice = user.toLowerCase()
 
-
+//this can be shortened
     if (computerChoice == getUserChoice){
-        return "draw"
+        return `draw\nUser points: ${userScore} Computer points: ${compScore}`
 
     }else if (getUserChoice === "rock" && computerChoice === "scissors"){
         userScore +=1
-        return "User won the round"
+        return `User won the round\nUser points: ${userScore} Computer points: ${compScore}`
 
     }else if (getUserChoice === "rock" && computerChoice === "paper"){
         compScore +=1
-        return "Computer won the round"
+        return `Computer won the round.\nUser points: ${userScore} Computer points: ${compScore}`
 
     }else if (getUserChoice === "paper" && computerChoice === "rock"){
         userScore +=1
-        return "User won the round"
+        return `User won the round\nUser points: ${userScore} Computer points: ${compScore}`
 
     }else if (getUserChoice === "paper" && computerChoice === "scissors"){
         compScore +=1
-        return "Computer won the round"
+        return `Computer won the round.\nUser points: ${userScore} Computer points: ${compScore}`
 
     }else if (getUserChoice === "scissors" && computerChoice === "paper"){
         userScore +=1
-        return "User won the round"
+        return `User won the round\nUser points: ${userScore} Computer points: ${compScore}`
 
     }else if (getUserChoice === "scissors" && computerChoice === "rock"){
         compScore +=1
-        return "Computer won the round"
+        return `Computer won the round.\nUser points: ${userScore} Computer points: ${compScore}`
     }
- 
-
-   
 }
 
 // display the winner
-function winner(){
+function checkWinner(){
     if (userScore > compScore){
         return "User is just better"
     }else if (userScore < compScore){
         return "Computer is skill diff"
     }else{
-        return "draw"
+        return "Draw, both of you are noobs!"
     }
 }
-
-
 
 // need to loop the game 5 times
 function game(){
     for(let x = 0; x < 5; x++){
         console.log(round())
-        console.log(`User points: ${userScore} Computer points: ${compScore}`)
+        //return round()?
+        //console.log(`User points: ${userScore} Computer points: ${compScore}`)
+        
         
         
     }
-    console.log(winner())
+    //return checkWinner()?
 }
 
-
-
 game()
-
-
-
-
-
-
+console.log(checkWinner())
 
 
 
