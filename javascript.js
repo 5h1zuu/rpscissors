2707192110f33e21408c775e29b044951f47bb07
Shizuu <coolcoledude111@gmail.com>
2022-10-04 14:47:30 +0100

    javascript.js

--- javascript.js
+++ javascript.js
@@ -15,17 +15,15 @@ function getComputerChoice(){
 }
 
 
-let getUserChoice = ""
+getUserChoice = ""
 
-
-
-
-
+// need to create a function
+// need to get the user choice
+//function getUserChoice for validation
 
 // need to create the conditions for the game
 let round = function playRound(){
     let computerChoice = getComputerChoice()
-
 //this can be shortened
     if (computerChoice == getUserChoice){
         return `draw\nUser points: ${userScore} Computer points: ${compScore}`
@@ -33,7 +31,6 @@ let round = function playRound(){
     }else if (getUserChoice === "rock" && computerChoice === "scissors"){
         userScore +=1
         return `User won the round\nUser points: ${userScore} Computer points: ${compScore}`
-        
 
     }else if (getUserChoice === "rock" && computerChoice === "paper"){
         compScore +=1
@@ -57,59 +54,21 @@ let round = function playRound(){
     }
 }
 
-// display the winner
-/*function checkWinner(){
-    if (userScore > compScore){
-        return "User is just better"
-    }else if (userScore < compScore){
-        return "Computer is skill diff"
-    }else{
-        return "Draw, both of you are noobs!"
-    }
-}
 
-// need to loop the game 5 times
-function game(){
-    for(let x = 0; x < 5; x++){
-        
-        //return round()?
-        //console.log(`User points: ${userScore} Computer points: ${compScore}`)    
-    }
-    //return checkWinner()?
-}
-
-
-*/
-document.querySelector("#one").addEventListener("click", () =>{
+const btnone = document.querySelector('#one')
+btnone.addEventListener('click', () =>{
     getUserChoice = "rock"
+    round()
 })
 
-document.querySelector("#two").addEventListener("click", () =>{
+const btntwo = document.querySelector('#two')
+btntwo.addEventListener('click', () =>{
     getUserChoice = "paper"
+    round()
 })
 
-
-document.querySelector("#three").addEventListener("click", () =>{
+const btnthree = document.querySelector('#three')
+btnthree.addEventListener('click', () =>{
     getUserChoice = "scissors"
+    round()
 })
-
-
-const container = document.querySelector('#container')
-const btn = document.querySelectorAll('button');
-
-/*btn.forEach((button) => {
-    
-    button.addEventListener('click', () => {
-        
-        const result = document.createElement('p')
-        result.textContent = round()
-        console.log(result)
-        container.appendChild(result)
-        
-        
-
-    });
-    
-});
-*/
-
