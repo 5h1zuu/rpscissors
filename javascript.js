9439c1fc165b059c4ae66c11d9d3371a1ccab18c
Shizuu <coolcoledude111@gmail.com>
2022-10-05 12:55:16 +0100

    javascript.js

--- javascript.js
+++ javascript.js
@@ -1,6 +1,8 @@
 let compScore = 0
 let userScore = 0
-console.log(`User points: ${userScore} Computer points: ${compScore}`)
+let userPoints = document.getElementById('score')
+let compPoints = document.getElementById('comp')
+let message = document.getElementById('won')
 
 // need to get the computer choice
 function getComputer(){
@@ -15,71 +17,85 @@ function getComputer(){
 }
 
 let getUserChoice = ""
-
 let score = document.getElementById('score')
 let roundwin = document.getElementById('roundwin')
-
-
-
 
 
 let rockbtn = document.getElementById('one')
 rockbtn.addEventListener('click', () => {
     getUserChoice = "rock"
     getComputer()
-    round()
+    game()
+    
+    
 })
 
 let paperbtn = document.getElementById('two')
 paperbtn.addEventListener('click', () => {
     getUserChoice = "paper"
     getComputer()
-    round()
+    game()
+    
+
 })
 
 let scissorsbtn = document.getElementById('three')
 scissorsbtn.addEventListener('click', () => {
     getUserChoice = "scissors"
     getComputer()
-    round()
+    game()
+   
 })
-
-
-let draw = document.createElement('p')
-draw.textContent = "Draw"
-
-let win = document.createElement('p')
-win.textContent = "You won"
-
-let lose = document.createElement('p')
-lose.textContent = "You lose"
-
 
 
 
 function round(){
-    if (getUserChoice == getComputer()){
-        roundwin.append(draw)
+        if (getUserChoice == getComputer()){
+            message.textContent = ('Draw')
 
-    }else if (getUserChoice === "rock" && getComputer() === "scissors"){
-        roundwin.append(win)
+        }else if (getUserChoice === "rock" && getComputer() === "scissors"){
+            message.textContent = ('You win')
+            ++userScore
 
-    }else if (getUserChoice === "rock" && getComputer() === "paper"){
-        roundwin.append(lose)
+        }else if (getUserChoice === "rock" && getComputer() === "paper"){
+            message.textContent = ('You lose')
+            ++compScore
 
-    }else if (getUserChoice === "paper" && getComputer() === "rock"){
-        roundwin.append(win)
+        }else if (getUserChoice === "paper" && getComputer() === "rock"){
+            message.textContent = ('You win')
+            ++userScore
 
-    }else if (getUserChoice === "paper" && getComputer() === "scissors"){
-        roundwin.append(lose)
+        }else if (getUserChoice === "paper" && getComputer() === "scissors"){
+            message.textContent = ('You lose')
+            ++compScore
 
-    }else if (getUserChoice === "scissors" && getComputer() === "paper"){
-        roundwin.append(win)
+        }else if (getUserChoice === "scissors" && getComputer() === "paper"){
+            message.textContent = ('You win')
+            ++userScore
 
-    }else if (getUserChoice === "scissors" && getComputer() === "rock"){
-        roundwin.append(lose)
-    }
+        }else if (getUserChoice === "scissors" && getComputer() === "rock"){
+            message.textContent = ('You lose')
+            ++compScore
+        }
+    userPoints.textContent = ("Your Score: " + userScore)
+    compPoints.textContent = ("Computer Score: " + compScore)
 }
 
 
+function game(){
+    round()
+    if (userScore == 5 ){
+        message.textContent = ("Game Over: The User Won")
+        userScore = 0
+        compScore = 0
+        userPoints.textContent = ("Your Score: " + userScore)
+        compPoints.textContent = ("Your Score: " + userScore)
 
+    }else if (compScore == 5){
+        message.textContent = ("Game Over: The Computer Won")
+        userScore = 0
+        compScore = 0
+        userPoints.textContent = ("Your Score: " + userScore)
+        compPoints.textContent = ("Computer Score: " + compScore)
+    }
+}
